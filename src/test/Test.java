34f52824ae7cb1b4deed37d22524c2abd17a6dcb
saeed-zh <gsilyp@gmail.com>
2015-12-31 17:35:01 +0330

    اولین testcase سایت سان مارکت

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,8 +1,10 @@
 package test;
 
+import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.fail;
@@ -16,22 +18,42 @@ public class Test {
 
     private WebDriver driver;
     private String baseUrl;
-    private boolean acceptNextAlert = true;
+
     private final StringBuffer verificationErrors = new StringBuffer();
 
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
-        baseUrl = "http://www.binaryhexconverter.com/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        baseUrl = "http://localhost:25557";
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
 
     public void uiTest() throws Exception {
-        System.out.println("hello");
-        driver.get(baseUrl + "/binary-to-decimal-converter");
-        driver.findElement(By.id("tabin")).clear();
-        driver.findElement(By.id("tabin")).sendKeys("100");
-        driver.findElement(By.cssSelector("input[type=\"button\"]")).click();
-        System.out.println("The result that fetch from browser is : "+driver.findElement(By.id("resulttxt")).getAttribute("value"));
+            System.out.println("beginning of test");
+        driver.get(baseUrl + "/Home/Index");
+            System.out.println("user being login");
+        // log in
+        driver.findElement(By.xpath(".//*[@id='loginOrRegister']/a[1]")).click();
+        driver.findElement(By.xpath(".//*[@id='inputUsernameRegister']")).sendKeys("tempuser"); // username
+        driver.findElement(By.xpath(".//*[@id='inputPasswordRegister']")).sendKeys("123"); // password
+        driver.findElement(By.xpath(".//*[@id='loginModal']/div[2]/form/input")).click();      // submit login
+        // search
+        driver.findElement(By.xpath(".//*[@id='SearchString']")).sendKeys("galaxy"); // type a search key for example galaxy
+        driver.findElement(By.xpath(".//*[@id='stickyNavbar']/div/div/div/div[1]/div/div/form/button")).click();
+        // add to cart
+        driver.findElement(By.xpath(".//*[@id='isotopeContainer']/div[2]/div/div[1]/div/div/a[2]")).click();
+        // save name of the product
+        String selectedProductName = driver.findElement(
+                By.xpath(".//*[@id='isotopeContainer']/div[2]/div/div[2]/div[2]/h5")).getText();
+        // verify cart content
+        driver.findElement(By.xpath(".//*[@id='cartContainer']")).click();
+        List<WebElement> products = driver.findElement(
+                By.xpath(".//*[@id='cartContainer']/div[2]")).findElements(By.className("item-in-cart"));
+        // verify count of products in cart
+        Assert.assertEquals(1,products.size());
+        // verify name of product in list that must be equal with the selected product
+        String curProductName = products.get(0).findElement(
+                By.xpath(".//*[@id='cartContainer']/div[2]/div[1]/div[2]/strong/a")).getText();
+        Assert.assertEquals(curProductName, selectedProductName);
 
     }
 
@@ -40,39 +62,6 @@ public class Test {
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
         }
     }
 
