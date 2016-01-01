89f1e990639c8293f5301f5460be7b48f74d14c5
saeed-zh <gsilyp@gmail.com>
2016-01-01 21:44:14 +0330

    اعمال برخی اصلاحات برای اینکه سلنیوم به صورت اتوماتیک بتواند روی کالا hover کند و کالا را انتخاب سپس با hover روی سبد کالا، کالای انتخاب شده را بررسی کند

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,9 @@ package test;
 import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -30,30 +33,47 @@ public class Test {
     public void uiTest() throws Exception {
             System.out.println("beginning of test");
         driver.get(baseUrl + "/Home/Index");
-            System.out.println("user being login");
         // log in
+        System.out.println("user being login");
         driver.findElement(By.xpath(".//*[@id='loginOrRegister']/a[1]")).click();
         driver.findElement(By.xpath(".//*[@id='inputUsernameRegister']")).sendKeys("tempuser"); // username
         driver.findElement(By.xpath(".//*[@id='inputPasswordRegister']")).sendKeys("123"); // password
         driver.findElement(By.xpath(".//*[@id='loginModal']/div[2]/form/input")).click();      // submit login
+
+        Assert.assertEquals("tempuser", driver.findElement(By.xpath(".//*[@id='loginOrRegister']/a[1]")).getText());
+        System.out.println("user login successfully");
         // search
+        System.out.println("user being search");
         driver.findElement(By.xpath(".//*[@id='SearchString']")).sendKeys("galaxy"); // type a search key for example galaxy
         driver.findElement(By.xpath(".//*[@id='stickyNavbar']/div/div/div/div[1]/div/div/form/button")).click();
         // add to cart
+        System.out.println("search page is opening");
+        System.out.println("adding galaxy s6 to cart");
+
+        Actions action = new Actions(driver);
+        WebElement mouseOverOnProduct = driver.findElement(By.xpath(".//*[@id='isotopeContainer']/div[2]/div/div[1]/div/img"));
+        action.moveToElement(mouseOverOnProduct).build().perform();
         driver.findElement(By.xpath(".//*[@id='isotopeContainer']/div[2]/div/div[1]/div/div/a[2]")).click();
+
         // save name of the product
+        System.out.println("verifying cart");
         String selectedProductName = driver.findElement(
                 By.xpath(".//*[@id='isotopeContainer']/div[2]/div/div[2]/div[2]/h5")).getText();
+
         // verify cart content
-        driver.findElement(By.xpath(".//*[@id='cartContainer']")).click();
+        action.moveToElement(driver.findElement(By.xpath(".//*[@id='cartContainer']"))).build().perform();
+        //driver.findElement(By.xpath(".//*[@id='cartContainer']")).click();
         List<WebElement> products = driver.findElement(
                 By.xpath(".//*[@id='cartContainer']/div[2]")).findElements(By.className("item-in-cart"));
         // verify count of products in cart
         Assert.assertEquals(1,products.size());
         // verify name of product in list that must be equal with the selected product
-        String curProductName = products.get(0).findElement(
+
+        String curProductName = driver.findElement(
                 By.xpath(".//*[@id='cartContainer']/div[2]/div[1]/div[2]/strong/a")).getText();
+        System.out.println("product in list is: " + curProductName);
         Assert.assertEquals(curProductName, selectedProductName);
+        System.out.println("test is complete successfully");
 
     }
 
